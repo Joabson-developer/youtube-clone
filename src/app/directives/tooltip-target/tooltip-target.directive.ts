e713d093d674f3e5f6fe57635d7e167eb22d0b28
Joabson-developer <83815302+Joabson-developer@users.noreply.github.com>
2022-04-18 13:21:59 -0300

    Ajuste position tooltip

--- src/app/directives/tooltip-target/tooltip-target.directive.ts
+++ src/app/directives/tooltip-target/tooltip-target.directive.ts
@@ -11,22 +11,28 @@ export class TooltipTargetDirective {
   ngOnInit(): void {}
 
   ngAfterViewInit(): void {
+    const elementNative = this.elementRef.nativeElement;
+
     const elementTarget = <HTMLElement>(
       document.getElementById(`${this.target}`)
     );
-    const elementNative = this.elementRef.nativeElement;
 
-    elementTarget.style.left = `${elementNative.offsetLeft + 30}px`;
+    elementTarget.style.left = `${
+      elementNative.offsetLeft +
+      45 +
+      elementNative.clientWidth / 2 -
+      elementTarget.clientWidth / 2
+    }px`;
     elementTarget.style.top = `${elementNative.offsetHeight + 30}px`;
 
     elementNative.addEventListener('mouseenter', (e: Event) => {
       e.stopPropagation();
-      elementTarget.style.display = 'block';
+      elementTarget.style.opacity = '1';
     });
 
     elementNative.addEventListener('mouseout', (e: Event) => {
       e.stopPropagation();
-      elementTarget.style.display = 'none';
+      elementTarget.style.opacity = '0';
     });
   }
 }
