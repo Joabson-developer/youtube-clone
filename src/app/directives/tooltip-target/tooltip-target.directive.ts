a6d4d8974f891985b0fc43e861f5cf7298fbef49
Joabson-developer <83815302+Joabson-developer@users.noreply.github.com>
2022-04-18 17:22:29 -0300

    Ajustes css

--- src/app/directives/tooltip-target/tooltip-target.directive.ts
+++ src/app/directives/tooltip-target/tooltip-target.directive.ts
@@ -17,22 +17,35 @@ export class TooltipTargetDirective {
       document.getElementById(`${this.target}`)
     );
 
-    elementTarget.style.left = `${
-      elementNative.offsetLeft +
-      45 +
-      elementNative.clientWidth / 2 -
-      elementTarget.clientWidth / 2
-    }px`;
-    elementTarget.style.top = `${elementNative.offsetHeight + 30}px`;
+    // elementTarget.style.left = `${
+    //   elementNative.offsetLeft +
+    //   45 +
+    // elementNative.clientWidth / 2 -
+    // elementTarget.clientWidth / 2
+    // }px`;
+
+    this.updatePosition(elementTarget, elementNative);
 
     elementNative.addEventListener('mouseenter', (e: Event) => {
-      e.stopPropagation();
+      this.updatePosition(elementTarget, elementNative);
       elementTarget.style.opacity = '1';
     });
 
     elementNative.addEventListener('mouseout', (e: Event) => {
-      e.stopPropagation();
       elementTarget.style.opacity = '0';
     });
+  }
+
+  private updatePosition(
+    elementTarget: HTMLElement,
+    elementNative: HTMLElement
+  ) {
+    elementTarget.style.inset = `${
+      elementNative.offsetHeight + 30
+    }px auto auto ${
+      elementNative.offsetLeft +
+      elementNative.clientWidth / 2 -
+      elementTarget.clientWidth / 2
+    }px`;
   }
 }
